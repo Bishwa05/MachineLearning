38e6a754684681d14dedf2487c40a58028a23c1e
Bishwa <bishwa.nayak@sap.com>
2019-09-27 11:40:08 +0530

    learning from SAP Ariba class.

---
+++ Ariba ML/myFile.py
@@ -0,0 +1,21 @@
+def rev(l):
+    """
+    This function reverse a list
+    :param l:
+    :return:
+    """
+    return l[::-1]
+
+def head(l):
+    """
+    This function returns first 10 values of a list
+    :param l:
+    :return:
+    """
+    return l[:10]
+x = 10
+
+print("I am in my file")
+
+print(" value of x is {}".format(x))
+
---
+++ Ariba ML/test.py
@@ -0,0 +1,4 @@
+import myFile
+
+l = [10,20,30]
+print(myFile.rev(l))
